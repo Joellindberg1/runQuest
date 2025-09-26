636c5c79857cb75e382819d1339870abd8310096
Joellindberg1 <Joellindberg23@gmail.com>
2025-09-26 15:53:46 +0200

    setup frontend build configuration

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,20 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  publicDir: "public",
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
